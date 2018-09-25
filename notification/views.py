e96e53eb9f3e821a3d0c2d3c79d084dd9b81453e
Matheus <matheusrf96@gmail.com>
2018-09-25 16:23:18 -0300

    Implementação do limite de validade das notificações

--- notification/views.py
+++ notification/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.core import serializers
+import datetime
 
 # Create your views here.
 from .models import Notification
 
+""" def validate_number_date(num):
+    if num < 10:
+        num = '0' + str(num) """
+
 def api_notification(request):
-    serialized_obj = serializers.serialize('json', Notification.objects.all())
+    """ today = datetime.datetime.today()
+
+    serialized_obj = serializers.serialize('json', Notification.objects.filter(
+        send_date__year=today.year,
+        send_date__month=validate_number_date(today.month),
+        send_date__day=validate_number_date(today.day),
+    )) """
+
+    serialized_obj = serializers.serialize('json', Notification.objects.filter(
+        send_date__range=(
+            datetime.datetime.now() - datetime.timedelta(minutes=15),
+            datetime.datetime.now()
+        )
+    ))
 
     return render(request, 'notification.html', { 'json': serialized_obj })
