65132cf2025599f9b75fc11ba1117839104af3f4
Matheus <matheusrf96@gmail.com>
2018-09-26 12:41:33 -0300

    Correção view api_notification

--- notification/views.py
+++ notification/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.core import serializers
+from django.http import HttpResponse
 import datetime
 
 # Create your views here.
@@ -11,19 +12,11 @@ from .models import Notification
         num = '0' + str(num) """
 
 def api_notification(request):
-    """ today = datetime.datetime.today()
-
-    serialized_obj = serializers.serialize('json', Notification.objects.filter(
-        send_date__year=today.year,
-        send_date__month=validate_number_date(today.month),
-        send_date__day=validate_number_date(today.day),
-    )) """
-
-    serialized_obj = serializers.serialize('json', Notification.objects.filter(
+    obj_list = serializers.serialize('json', Notification.objects.filter(
         send_date__range=(
             datetime.datetime.now() - datetime.timedelta(minutes=15),
             datetime.datetime.now()
         )
     ))
 
-    return render(request, 'notification.html', { 'json': serialized_obj })
+    return HttpResponse(obj_list, content_type="application/json")
